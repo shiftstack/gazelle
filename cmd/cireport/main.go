shiftstack/gazelle

commit d6ad3d7ea1e32d92eb97d668277308f66d20d9c3

Allow running for all jobs if `-job` param is unset
With this, updating the spreadsheet with latest results for all jobs is
as simple as running `./cireport`.

cmd/cireport/main.go
@@ -20,60 +20,88 @@ var (
 	username    string
 )
 
+var valid_jobs = []string{
+	"release-openshift-ocp-installer-e2e-openstack-4.4",
+	"release-openshift-ocp-installer-e2e-openstack-serial-4.4",
+	"release-openshift-ocp-installer-e2e-openstack-4.3",
+	"release-openshift-ocp-installer-e2e-openstack-serial-4.3",
+	"release-openshift-ocp-installer-e2e-openstack-4.2",
+	"release-openshift-ocp-installer-e2e-openstack-serial-4.2",
+	"release-openshift-origin-installer-e2e-openstack-4.2",
+	"release-openshift-origin-installer-e2e-openstack-serial-4.2",
+}
+
 func main() {
 	client := gsheets.NewClient()
-	sheet := gsheets.Sheet{
-		JobName: fullJobName,
-		Client:  &client,
+
+	var jobs []string
+	if fullJobName == "" {
+		jobs = valid_jobs
+	} else {
+		jobs = append(jobs, fullJobName)
 	}
 
-	if jobIDs == "" {
-		lowerBound := sheet.GetLatestId() + 1
-		upperBound := prow.GetLatestId(fullJobName)
-		jobIDs = fmt.Sprintf("%d-%d", lowerBound, upperBound)
-	}
-
-	ids, err := sequence.Int(jobIDs)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, i := range ids {
-		j := job.Job{
-			FullName: fullJobName,
-			ID:       strconv.Itoa(i),
+	for _, jobName := range jobs {
+		sheet := gsheets.Sheet{
+			JobName: jobName,
+			Client:  &client,
 		}
 
-		result, err := j.Result()
-		if err == nil {
-			j.ComputedResult = result
-		} else {
-			j.ComputedResult = "Pending"
-		}
-
-		var (
-			testFailures  []string
-			infraFailures []string
-		)
-		for failure := range rca.Find(j) {
-			if failure.IsInfra() {
-				infraFailures = append(infraFailures, failure.String())
+		var realJobIDs string
+		if jobIDs == "" {
+			lowerBound := sheet.GetLatestId() + 1
+			upperBound := prow.GetLatestId(jobName)
+			if lowerBound < upperBound {
+				realJobIDs = fmt.Sprintf("%d-%d", lowerBound, upperBound)
+			} else if lowerBound == upperBound {
+				realJobIDs = fmt.Sprintf("%d", upperBound)
+			} else {
+				continue
 			}
-			testFailures = append(testFailures, failure.String())
 		}
 
-		j.RootCause = testFailures
-		if len(infraFailures) > 0 {
-			j.RootCause = infraFailures
-			j.ComputedResult = "INFRA FAILURE"
+		ids, err := sequence.Int(realJobIDs)
+		if err != nil {
+			panic(err)
 		}
 
-		sheet.AddRow(j, username)
+		for _, i := range ids {
+			j := job.Job{
+				FullName: jobName,
+				ID:       strconv.Itoa(i),
+			}
+
+			result, err := j.Result()
+			if err == nil {
+				j.ComputedResult = result
+			} else {
+				j.ComputedResult = "Pending"
+			}
+
+			var (
+				testFailures  []string
+				infraFailures []string
+			)
+			for failure := range rca.Find(j) {
+				if failure.IsInfra() {
+					infraFailures = append(infraFailures, failure.String())
+				}
+				testFailures = append(testFailures, failure.String())
+			}
+
+			j.RootCause = testFailures
+			if len(infraFailures) > 0 {
+				j.RootCause = infraFailures
+				j.ComputedResult = "INFRA FAILURE"
+			}
+
+			sheet.AddRow(j, username)
+		}
 	}
 }
 
 func init() {
-	flag.StringVar(&fullJobName, "job", "", "Full name of the test job (e.g. release-openshift-ocp-installer-e2e-openstack-serial-4.4)")
+	flag.StringVar(&fullJobName, "job", "", "Full name of the test job (e.g. release-openshift-ocp-installer-e2e-openstack-serial-4.4). All known jobs if unset.")
 	flag.StringVar(&jobIDs, "id", "", "Job IDs. If unset, it consists of all new runs since last time the spreadsheet was updated.")
 
 	flag.StringVar(&username, "user", os.Getenv("CIREPORT_USER"), "Username to use for CI Cop")

commit 81c642dfb203bc58a612699cc8a9903319419ea1

Fix --id option
Commit d6ad3d7 introduced a regression where passing the job IDs with
the `-id` flag would result in an empty list.

This commit fixes it by setting the realJobIDs variable correctly.

cmd/cireport/main.go
@@ -47,8 +47,8 @@ func main() {
 			Client:  &client,
 		}
 
-		var realJobIDs string
-		if jobIDs == "" {
+		realJobIDs := jobIDs
+		if realJobIDs == "" {
 			lowerBound := sheet.GetLatestId() + 1
 			upperBound := prow.GetLatestId(jobName)
 			if lowerBound < upperBound {