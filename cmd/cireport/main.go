b96b31aaedea574ed926a5dd6a4995e8f46f338f
Pierre Prinetti <pierreprinetti@redhat.com>
2020-02-28 15:46:19 +0100

    Run jobs concurrently

--- cmd/cireport/main.go
+++ cmd/cireport/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/user"
 	"strconv"
+	"sync"
 
 	"github.com/pierreprinetti/go-sequence"
 	"github.com/shiftstack/gazelle/pkg/gsheets"
@@ -29,6 +30,65 @@ var valid_jobs = []string{
 	"release-openshift-ocp-installer-e2e-openstack-serial-4.2",
 }
 
+func runJob(jobName, jobIDs string, client *gsheets.Client) {
+	sheet := gsheets.Sheet{
+		JobName: jobName,
+		Client:  client,
+	}
+
+	if jobIDs == "" {
+		lowerBound := sheet.GetLatestId() + 1
+		upperBound := prow.GetLatestId(jobName)
+		if lowerBound < upperBound {
+			jobIDs = fmt.Sprintf("%d-%d", lowerBound, upperBound)
+		} else if lowerBound == upperBound {
+			jobIDs = fmt.Sprintf("%d", upperBound)
+		} else {
+			return
+		}
+	}
+	fmt.Printf("Updating %s with results from jobs %s\n", jobName, jobIDs)
+
+	ids, err := sequence.Int(jobIDs)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, i := range ids {
+		fmt.Printf("%s %v\n", jobName, i)
+		j := job.Job{
+			FullName: jobName,
+			ID:       strconv.Itoa(i),
+		}
+
+		result, err := j.Result()
+		if err == nil {
+			j.ComputedResult = result
+		} else {
+			j.ComputedResult = "Pending"
+		}
+
+		var (
+			testFailures  []string
+			infraFailures []string
+		)
+		for failure := range rca.Find(j) {
+			if failure.IsInfra() {
+				infraFailures = append(infraFailures, failure.String())
+			}
+			testFailures = append(testFailures, failure.String())
+		}
+
+		j.RootCause = testFailures
+		if len(infraFailures) > 0 {
+			j.RootCause = infraFailures
+			j.ComputedResult = "INFRA FAILURE"
+		}
+
+		sheet.AddRow(j, username)
+	}
+}
+
 func main() {
 	client := gsheets.NewClient()
 
@@ -39,66 +99,15 @@ func main() {
 		jobs = append(jobs, fullJobName)
 	}
 
+	var wg sync.WaitGroup
 	for _, jobName := range jobs {
-		sheet := gsheets.Sheet{
-			JobName: jobName,
-			Client:  &client,
-		}
-
-		realJobIDs := jobIDs
-		if realJobIDs == "" {
-			lowerBound := sheet.GetLatestId() + 1
-			upperBound := prow.GetLatestId(jobName)
-			if lowerBound < upperBound {
-				realJobIDs = fmt.Sprintf("%d-%d", lowerBound, upperBound)
-			} else if lowerBound == upperBound {
-				realJobIDs = fmt.Sprintf("%d", upperBound)
-			} else {
-				// This sheet is already up-to-date
-				continue
-			}
-		}
-		fmt.Printf("Updating %s with results from jobs %s\n", jobName, realJobIDs)
-
-		ids, err := sequence.Int(realJobIDs)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, i := range ids {
-			fmt.Printf("%s %v\n", jobName, i)
-			j := job.Job{
-				FullName: jobName,
-				ID:       strconv.Itoa(i),
-			}
-
-			result, err := j.Result()
-			if err == nil {
-				j.ComputedResult = result
-			} else {
-				j.ComputedResult = "Pending"
-			}
-
-			var (
-				testFailures  []string
-				infraFailures []string
-			)
-			for failure := range rca.Find(j) {
-				if failure.IsInfra() {
-					infraFailures = append(infraFailures, failure.String())
-				}
-				testFailures = append(testFailures, failure.String())
-			}
-
-			j.RootCause = testFailures
-			if len(infraFailures) > 0 {
-				j.RootCause = infraFailures
-				j.ComputedResult = "INFRA FAILURE"
-			}
-
-			sheet.AddRow(j, username)
-		}
+		wg.Add(1)
+		go func(jobName string) {
+			runJob(jobName, jobIDs, &client)
+			wg.Done()
+		}(jobName)
 	}
+	wg.Wait()
 }
 
 func init() {
